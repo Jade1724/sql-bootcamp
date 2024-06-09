06f9f337aef7f656d5ce9869a5ef7aec47a15875
Jade1724 <haruka.i1724@gmail.com>
2024-06-10 09:32:08 +1200

    UPDATE query

--- create-db-table.sql
+++ create-db-table.sql
@@ -33,3 +33,20 @@ VALUES ('President');
 
 INSERT INTO account_job(user_id, job_id, hire_date)
 VALUES (1, 1, CURRENT_TIMESTAMP);
+
+-- UPDATE
+
+UPDATE account 
+SET last_login = CURRENT_TIMESTAMP;
+
+UPDATE account
+SET last_login = created_on;
+
+UPDATE account_job
+SET hire_date = account.created_on
+FROM account
+WHERE account_job.user_id = account.user_id;
+
+UPDATE account
+SET last_login = CURRENT_TIMESTAMP
+RETURNING email, created_on, last_login
