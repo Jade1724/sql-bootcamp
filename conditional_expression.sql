ce7216aca0c129a16f94e8f92971ea1d13476454
Jade1724 <haruka.i1724@gmail.com>
2024-07-01 09:34:45 +1200

    Using Views

--- conditional_expression.sql
+++ conditional_expression.sql
@@ -95,3 +95,20 @@ SELECT (
 	NULLIF(SUM(CASE WHEN department = 'B' THEN 1 ELSE 0 END), 0)
 ) AS department_ratio
 FROM depts;
+
+
+Views
+
+CREATE VIEW customer_info AS 
+SELECT first_name, last_name, address FROM customer
+INNER JOIN address
+ON customer.address_id = address.address_id;
+
+CREATE OR REPLACE VIEW customer_info AS
+SELECT first_name, last_name, address, district FROM customer
+INNER JOIN address
+ON customer.address_id = address.address_id;
+
+DROP VIEW IF EXISTS customer_info;
+
+ALTER VIEW customer_info RENAME TO c_info;
